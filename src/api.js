e450140e6dd45e3edbda92a15e8c1ea5d2e8b9ca
Yash Dani <yash.dani0406@gmail.com>
2023-03-07 23:41:54 +0530

    First commit

---
+++ src/api.js
@@ -0,0 +1,18 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b697df5fa0msh795952d01049229p17f358jsn69d2c53ade86',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "ef8a11ffc1984750c64bd182690a0431";
+
+// fetch('/adminDivisions', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
